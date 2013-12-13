dbe6860f7e5b3d4ccd09b232c520531081f1ca3c
simpleframework <cknet@126.com>
2013-12-13 13:15:24 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/msg/MessageException.java
+++ src/main/java/net/simpleframework/module/msg/MessageException.java
@@ -1,6 +1,6 @@
 package net.simpleframework.module.msg;
 
-import net.simpleframework.common.th.RuntimeExceptionEx;
+import net.simpleframework.ctx.ModuleException;
 
 /**
  * Licensed under the Apache License, Version 2.0
@@ -8,7 +8,7 @@ import net.simpleframework.common.th.RuntimeExceptionEx;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public class MessageException extends RuntimeExceptionEx {
+public class MessageException extends ModuleException {
 
 	public MessageException(final String msg, final Throwable cause) {
 		super(msg, cause);
@@ -18,8 +18,8 @@ public class MessageException extends RuntimeExceptionEx {
 		return _of(MessageException.class, msg);
 	}
 
-	public static RuntimeException of(final Throwable throwable) {
-		return _of(MessageException.class, null, throwable);
+	public static MessageException of(final Throwable throwable) {
+		return (MessageException) _of(MessageException.class, null, throwable);
 	}
 
 	private static final long serialVersionUID = 8964555866578533829L;

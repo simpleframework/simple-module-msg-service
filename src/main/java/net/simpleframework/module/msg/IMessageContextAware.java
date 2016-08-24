e09e1906bf54dc520aa553d8f01c12aae0947253
CHENK <CHENK@CHENK-PC>
2016-08-24 22:35:22 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/module/msg/IMessageContextAware.java
+++ src/main/java/net/simpleframework/module/msg/IMessageContextAware.java
@@ -6,10 +6,14 @@ import net.simpleframework.ctx.ModuleContextFactory;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885)
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public interface IMessageContextAware extends IModuleContextAware {
 
 	static IMessageContext messageContext = ModuleContextFactory.get(IMessageContext.class);
+
+	/* 短信服务 */
+	static ISMSService _smsService = messageContext.getSMSService();
 }
